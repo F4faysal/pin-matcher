231cfd9ce95ee4a4551950e1e0ecde0807e96a46
F4faysal <faysal15-14743@diu.edu.bd>
2022-08-25 15:47:49 +0600

    add a cler and backspace

--- pin.js
+++ pin.js
@@ -14,3 +14,39 @@ function genaratPin (){
     const random = Math.round(Math.random()*10000) ;
     return random ;
 }
+
+
+document.getElementById('generate-pin-btn').addEventListener('click', function(){
+    const pin = getPin();
+    const viewPin = document.getElementById('form-control');
+    viewPin.value = pin;
+})
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    
+    const number = event.target.innerText ;
+    const fild =  document.getElementById('cal-form-control')
+    const privaseNuber = fild.value ;
+    if (isNaN(number)){
+        if (number === 'C' ){
+            fild.value = '';
+            }
+        else if(number === '<') {
+            const  digit = privaseNuber.split('');
+            digit.pop();
+            const remaintDigit = digit.join('')
+            console.log(remaintDigit)
+        }
+
+    }
+    else{
+      
+      const carrentNumber = privaseNuber + number; 
+      fild.value = carrentNumber;
+    }
+    
+ 
+
+    
+  
+})
